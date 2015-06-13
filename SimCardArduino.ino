25bc1f136803613aa8f8beacaaff9cd93c291e56
Benjamin Renz <appswert@gmail.com>
2015-06-13 19:11:12 +0200

    Bugfix 1.1
    Additional Comments & other improvements

--- SimCardArduino.ino
+++ SimCardArduino.ino
@@ -1,6 +1,7 @@
 #define Clk 3
 #define IO 4
 #define Rst 5
+#define Vcc 6
 unsigned long maxWait = 372000;
 unsigned long nextBitTime;
 unsigned long endTime;
@@ -14,45 +15,48 @@ boolean startbitFound = 0;
 void setup() {
   Serial.begin(9600);
   pinMode(Clk, OUTPUT);
-  pinMode(IO, OUTPUT);
   pinMode(Rst, OUTPUT);
-
-  digitalWrite(Clk, LOW);
-  digitalWrite(IO, LOW);
-  digitalWrite(Rst, LOW);
+  pinMode(Vcc, OUTPUT);
   pinMode(IO, INPUT);
+  digitalWrite(Rst, HIGH);
+  digitalWrite(Vcc, LOW);
+
 
   delay(200);
-  Serial.print("Starting .");
-  delay(200);
-  Serial.print(".");
+  Serial.println("1. Starting clock generation");
 
   TCCR2A = B00100011;
   TCCR2B = B00001001;
   OCR2A = 3;
   OCR2B = 1;
+  Serial.println("2. Card Powerup");
+  digitalWrite(Vcc, HIGH);
   delayMicroseconds(100);
-  digitalWrite(Rst, HIGH);
-  delayMicroseconds(150);
   digitalWrite(Rst, LOW);
+  delayMicroseconds(150);
+  digitalWrite(Rst, HIGH);
+  delay(4);
+  Serial.println("3. Reset sent");
 
-
-  // falling edge of start bit
-  Serial.println("");
-   Serial.println("wait for Startbit");
+  // falling edge of first bit
   while (digitalRead(IO) == HIGH) {}
-  // time for 1st data bit
-  nextBitTime = micros() + etu + etu / 2;
-  // timout calc
-  endTime = micros() + maxWait;
-    Serial.println("found Startbit");
-while (digitalRead(IO) == LOW) {}
-  start = micros();
+  // rising edge of first bit
+  while (digitalRead(IO) == LOW) {}
+  // falling edge of second bit
   while (digitalRead(IO) == HIGH) {}
-  etu = (micros() - start) / 3;
+  // rising edge of second bit
+  while (digitalRead(IO) == LOW) {start = micros();}
+  // falling edge of third bit
+do  { etu = ((micros() - start) / 3)+2;}
+while (digitalRead(IO) == HIGH);
+  Serial.println("4. Calc Etu");
+  Serial.print("etu:    ");
+  Serial.println(etu);
   guardTime = etu * 2;
-  wwt = 1920 * etu;
-  maxwwt = wwt + 480 * etu;
+  //wwt = 1920 * etu;
+  //maxwwt = wwt + 480 * etu;
+  delay(10);
+   nextBitTime = micros() + etu + etu / 2;
   endTime = micros() + maxWait;
   for (int bit = 8; bit = 0; bit--) {
     while (micros() < nextBitTime) {
