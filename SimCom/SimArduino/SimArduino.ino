74af2faf76b2b481757ec5cbb527eb601fd14897
Benjamin Renz <appswert@gmail.com>
2015-06-22 19:44:07 +0200

    Patch 2015.06.2015.19.44

--- SimCom/SimArduino/SimArduino.ino
+++ SimCom/SimArduino/SimArduino.ino
@@ -1,21 +1,25 @@
 #define Clk 3
 #define IO 4
 #define Rst 5
-#define MOSI 0
-#define MISO 0
-#define SCK 
-void setup(
-Serial.begin(9600)
+void setup(){
+noInterrupts();
+Serial.begin(9600);
 pinMode(Clk, OUTPUT);
 pinMode(Rst, OUTPUT);
 pinMode(IO, INPUT);
-pinMode()
+pinMode(SCK, INPUT);
+pinMode(MOSI, INPUT);
+pinMode(MISO, OUTPUT);
 TCCR2A = B00100011;
 TCCR2B = B00001001;
 OCR2A = 3;
 OCR2B = 1;
-Serial.println("Startup finished... Waiting for SPI from Master")
-SPCR = 
-SPIF
+Serial.println("Startup finished... Waiting for SPI from Master");
 
-)
+SPCR = (1<<SPE);
+
+do{
+SPDR = B10000000;
+while(!(SPSR & (1<<SPIF)));
+} while (SPDR !=B01000000);
+}
