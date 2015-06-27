e7cfd7ce8dd52069998b55bc6310311210149a75
Benjamin Renz <appswert@gmail.com>
2015-06-27 16:24:32 +0200

    Patch 8

--- SimCom/SimArduino/SimArduino.ino
+++ SimCom/SimArduino/SimArduino.ino
@@ -1,4 +1,12 @@
-byte clr;
+byte dat;
+
+byte spi_recieve()
+{                 
+  while (!(SPSR & (1 << SPIF)))   // Wait the end of the transmission
+  {
+  };
+  return SPDR;                    // return the received byte
+}
 
 void setup() {
   Serial.begin(9600);
@@ -17,12 +25,15 @@ void setup() {
  
   Serial.println("Startup finished   by SIM");
    Serial.println("Waiting for 3G   by SIM");
- 
+ while(B10000000!=spi_recieve()){}
 
 }
 
 void loop() {
-  
+dat=spi_recieve();
+if(dat&B000000001==B00000001)
+{}
+
 
 }
 
