1bdde995d216ae134be325810e5a856bb5cf0ca0
Benjamin Renz <appswert@gmail.com>
2015-06-27 09:29:12 +0200

    Patch 6
    Test Com

--- SimCom/SimArduino/SimArduino.ino
+++ SimCom/SimArduino/SimArduino.ino
@@ -1,24 +1,28 @@
+#define Vcc 2
 #define Clk 3
 #define IO 4
 #define Rst 5
-void setup(){
-Serial.begin(9600);
-pinMode(Clk, OUTPUT);
-pinMode(Rst, OUTPUT);
-pinMode(IO, INPUT);
-pinMode(SCK, INPUT);
-pinMode(MOSI, INPUT);
-pinMode(MISO, OUTPUT);
-TCCR2A = B00100011;
-TCCR2B = B00001001;
-OCR2A = 3;
-OCR2B = 1;
-Serial.println("Startup finished by Sim");
-SPCR = (1<<SPE);
-do{SPDR = B10000000;
-while(!(SPSR & (1<<SPIF)));
-
-} while (SPDR != B01000000);
+void setup() {
+  Serial.begin(9600);
+  pinMode(Clk, OUTPUT);
+  pinMode(Rst, OUTPUT);
+  pinMode(IO, INPUT);
+  pinMode(SCK, INPUT);
+  pinMode(MOSI, INPUT);
+  pinMode(MISO, OUTPUT);
+  TCCR2A = B00100011;
+  TCCR2B = B00001001;
+  OCR2A = 3;
+  OCR2B = 1;
+  Serial.println("Startup finished by Sim");
 }
-SPDR = B10000000;
+void loop() {
+  SPCR = (1 << SPE);
+  SPDR = B01000000;
+  //Wait for first data transmition
+  while (!(SPSR & (1 << SPIF)));
+  if (SPDR == B10000000) {
+    Serial.println("Connection established by Sim");
+  }
+}
 
--- SimCom/_3GArduino/_3GArduino.ino
+++ SimCom/_3GArduino/_3GArduino.ino
@@ -1,16 +1,27 @@
+
+#define Vcc 2
+#define Clk 3
+#define IO 4
+#define Rst 5
+
 void setup() {
   Serial.begin(9600);
-  Serial.println("Startup finished by 3g");
   pinMode(MISO , INPUT);
   pinMode(SS , INPUT_PULLUP);
   pinMode(SCK , OUTPUT);
   pinMode(MOSI , OUTPUT);
-  do{SPDR = B10000000;
-  while(!(SPSR & (1<<SPIF)));
+  Serial.println("Startup finished by 3g");
+
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  //Set SPI data register available bit
+  SPDR = B10000000;
+  //Wait for first data transmition
+  while (!(SPSR & (1 << SPIF)));
+  if (SPDR == B01000000) {
+    Serial.println("Connection established by 3G");
+  }
 }
 
